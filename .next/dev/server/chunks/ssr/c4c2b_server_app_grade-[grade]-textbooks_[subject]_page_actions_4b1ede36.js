3f0264493cc767d93fa9a568aba2576fe8e08047
Muhidin3 <muhidinshemsu3@gmail.com>
2025-11-18 11:59:25 +0300

    Stop tracking .next folder

--- .next/dev/server/chunks/ssr/c4c2b_server_app_grade-[grade]-textbooks_[subject]_page_actions_4b1ede36.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/Desktop/web projects/exam buddy/exam-galaxy/.next-internal/server/app/grade-[grade]-textbooks/[subject]/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=c4c2b_server_app_grade-%5Bgrade%5D-textbooks_%5Bsubject%5D_page_actions_4b1ede36.js.map
