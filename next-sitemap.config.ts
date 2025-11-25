6bda71169bb41bed3acc6dc28efe5a2799605556
Muhidin3 <muhidinshemsu3@gmail.com>
2025-11-25 10:41:50 +0300

    .

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,8 +1,8 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://exam-galaxy.vercel.app",
-  // generateRobotsTxt: true,
-  // sitemapSize: 7000, // optional
+  generateRobotsTxt: true,
+  sitemapSize: 7000, // optional
   robotsTxtOptions: {
     policies: [
       {
