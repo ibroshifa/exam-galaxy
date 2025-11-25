043a3af9847047b193a9f8ed116ecbcc8ec444ae
Muhidin3 <muhidinshemsu3@gmail.com>
2025-11-25 06:55:08 +0300

    new sitemap for tesing

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://exam-galaxy.vercel.app",
   generateRobotsTxt: true,
-  sitemapSize: 7000, // optional
+  // sitemapSize: 7000, // optional
   robotsTxtOptions: {
     policies: [
       {
