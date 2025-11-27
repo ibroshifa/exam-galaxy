69feca1799a697c437a420c309c6906c1b386bf5
unknown <ibroshifa@gmail.com>
2025-11-27 21:18:44 +0300

    fix redeploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  // redeploy
   distDir: "out",// ← This forces output to /out instead of .next
   trailingSlash: true,
   images: {
